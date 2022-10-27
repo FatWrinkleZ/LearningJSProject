dccacfe45e1d17fa9831d84e68eb04928bb40f1e
FatWrinkleZ <mgiraldo2003@gmail.com>
2022-10-26 22:00:08 -0400

    finished game

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var width = canvas.width;
 var height = canvas.height;
 var amt_pieces = 1;
 const blockSize = 10;
+var score = 0;
+var ScoreText = document.getElementById("Score");
+var isPlaying = true;
+
+
 
 class Vector2{
     x = 0;
@@ -16,8 +21,17 @@ class Vector2{
     }
 }
 var pieces = [new Vector2(width/2, height/2)]; 
-var food;
-var direction;
+var food = new Vector2();
+var direction = new Vector2();
+/*
+const leftBtn = document.getElementById("LeftBtn");
+leftBtn.addEventListener('click', ButtonPress(-blockSize, 0));
+const rightBtn = document.getElementById("RightBtn");
+rightBtn.addEventListener('click', ButtonPress(blockSize, 0));
+const upBtn = document.getElementById("UpBtn");
+upBtn.addEventListener('click', ButtonPress(0, blockSize));
+const downBtn = document.getElementById("DownBtn");
+downBtn.addEventListener('click', ButtonPress(0, -blockSize));*/
 
 start();
 
@@ -45,35 +59,58 @@ function start(){
 }
 
 function update(){
-    context.fillStyle = "black";
-    context.fillRect(0,0,width, height);
-    context.fillStyle = "red";
-    context.fillRect(food.x, food.y, 10, 10);
-    context.fillStyle = "yellow";
-    pieces[0].x += direction.x;
-    pieces[0].y += direction.y;
-    if(pieces[0].x == food.x && pieces[0].y == food.y){
-        food = new Vector2((Math.random() * width/blockSize), (Math.random()* height/blockSize));
-        food.x *= blockSize;
-        food.y*=blockSize;
-        addPiece();
-    }
+    if(isPlaying){
+        context.fillStyle = "black";
+        context.fillRect(0,0,width, height);
+        context.fillStyle = "red";
+        context.fillRect(food.x, food.y, 10, 10);
+        context.fillStyle = "yellow";
+        pieces[0].x += direction.x;
+        pieces[0].y += direction.y;
+        if(pieces[0].x == food.x && pieces[0].y == food.y){
+            food = new Vector2((Math.random() * width/blockSize), (Math.random()* height/blockSize));
+            food.x *= blockSize;
+            food.y*=blockSize;
+            addPiece();
+            score++;
+            ScoreText.innerHTML = "Score : "+ score;
+        }
 
-    context.fillRect(pieces[0].x, pieces[0].y, 10, 10);
-    var lastElement = new Vector2(pieces[0].x, pieces[0].y);
-    for(var i = 1; i < amt_pieces; i++){
-        var tempElement = new Vector2(pieces[i].x, pieces[i].y);
-        pieces[i] = new Vector2(lastElement.x, lastElement.y);
-        lastElement = new Vector2(tempElement.x, tempElement.y);
-        context.fillRect(pieces[i].x, pieces[i].y, 10, 10);
-        //console.log("This was called " + i + " times");
-        /*if(pieces[0].x == pieces[i].x && pieces[0].y == pieces[i].y){
-            start();
-        }*/
+        context.fillRect(pieces[0].x, pieces[0].y, 10, 10);
+        var lastElement = new Vector2(pieces[0].x-direction.x, pieces[0].y-direction.y);
+        for(var i = 1; i < amt_pieces; i++){
+            var tempElement = new Vector2(pieces[i].x, pieces[i].y);
+            pieces[i] = new Vector2(lastElement.x, lastElement.y);
+            lastElement = new Vector2(tempElement.x, tempElement.y);
+            context.fillRect(pieces[i].x, pieces[i].y, 10, 10);
+
+            if(i > 1 && (pieces[0].x == pieces[i].x && pieces[0].y == pieces[i].y)){
+                //document.location.reload();
+                isPlaying = false;
+            }
+        }
+        if(pieces[0].x < 0 || pieces[0].x > width || pieces[0].y < 0 || pieces[0].y > height){
+            //document.location.reload();
+            isPlaying = false;
+        }
+    }else{
+        ScoreText.innerHTML = "Game Over Score : "+score;
     }
-    /*if(pieces[0].x < 0 || pieces[0].x > width || pieces[0].y < 0 || pieces[0].y > height){
-        start();
-    }*/
+}
+
+function Restart(){
+    document.location.reload();
+}
+
+function ButtonPress(dirx, diry){
+    console.log("Button pressed");
+    if(direction.x == 0  && dirx != 0){
+        direction.x = dirx;
+        direction.y = diry;
+    }else if(direction.y == 0 && diry != 0){
+        direction.x = dirx;
+        direction.y = diry;
+    }
 }
 
 function checkInput(event){
